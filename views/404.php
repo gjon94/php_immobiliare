8c3d0b412ba6ed5a2a9d1857c95feb0fa9bd89a3
gjon94 <gjongjoka94@hotmail.it>
2022-07-23 21:36:46 +0200

    firs in git hub test

--- views/404.php
+++ views/404.php
@@ -10,6 +10,8 @@
     <h1 class="display-1 text-center">Pagina non trovata</h1>
     <h3 class="display-1 text-center">Pagina non trovata</h3>
     <h3 class="display-1 text-center">Pagina non trovata</h3>
+    <h3 class="display-1 text-center">Pagina non trovata</h3>
+    <h3 class="display-1 text-center">Pagina non trovata</h3>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
